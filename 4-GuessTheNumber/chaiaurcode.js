104f0d03e4d92536c3b08a5f81ed4f4ee82efec2
ayushisingh3012 <76998756+ayushisingh3012@users.noreply.github.com>
2023-10-14 23:00:25 +0530

    guesstheno

--- 4-GuessTheNumber/chaiaurcode.js
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -17,3 +17,59 @@ let prevGuess=[];
 let numGuess = 1;
 
 let playGame=true;
+
+function validateGuess(guess) {
+    if(isNaN(guess)){
+        alert("Please enter a valid number");
+    }
+    else if(guess < 1){
+        alert("Please enter a number greater than 1");
+    }
+    else if(guess > 100){
+        alert("Please enter a number less than 100");
+    }
+    else{
+        prevGuess.push(guess);
+        if(numGuess === 11){
+            displayGuess(guess);
+            displayMessage(`Game Over. Random Number was ${randomNumber}`);
+            endGame(); 
+        }
+        else{
+            displayGuess(guess);
+            checkGuess(guess);
+        }
+    }
+}
+
+function checkGuess(guess) {
+    if(guess === randomNumber){
+        displayMessage(`You guessed it right`);
+        endGame();
+    }
+    else if(guess < randomNumber){
+        displayMessage(`Number you guessed is low`);
+    }
+    else if(guess > randomNumber){
+        displayMessage(`Number you guessed is high`);
+    }
+}
+
+function displayGuess(guess){
+    userInput.value = ''
+    guessSlot.innerHTML += `${guess}    `;
+    numGuess++;
+    remaining.innerHTML = `11-${numGuess}`;
+}
+
+function displayMessage(message){
+    lowOrHi.innerHTML = `<h2>${message}</h2>`;
+}
+
+function endGame(){
+
+}
+
+function newGame(){
+
+}
